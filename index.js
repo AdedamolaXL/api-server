e7da05df0a314e53b16082473c77e14bbd12057d
AdedamolaXL <adedamolaxl@outlook.com>
2024-02-13 14:53:02 +0100

    update: index.js

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ const cors = require('cors');
 const app = express();
 const port = 5000;
 
+app.use(express.json());
+
 const provider = new ethers.JsonRpcProvider('https://api.avax-test.network/ext/bc/C/rpc');
 const wallet = new ethers.Wallet(PRIVATE_KEY, provider);
 const Token_Address = '0x44d52D9F508F6F6378b9e9bbC74cEB4f394CbC32';
-const address = '0xAADa3A46D4A94593CaB32484279B86A4AfD149B0';
-const amount = 100;
+// const address = '0xAADa3A46D4A94593CaB32484279B86A4AfD149B0';
+// const amount = 100;
 const tokenContract = new ethers.Contract(Token_Address, Token_ABI, wallet);
 
 app.use(cors()); // Add this line to enable CORS for all routes
@@ -24,6 +26,11 @@ app.get('/', (req, res) => {
 app.post('/mintTokens', async (req, res) => {
     console.log('POST request received at /mintTokens');
     try {
+        const { amount, address} = req.body;
+        if (!amount || !address) {
+            throw new Error('Amount or address is missing in request body');
+        }
+
         const tokenMint = await tokenContract.mint(address, amount);
         const receipt = await tokenMint.wait();
         console.log('Transaction Receipt:', receipt);
